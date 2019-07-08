ca31e89b24d47ed1e5e68c2fc91f8a82d11b2cfb
Demi-hero <scott.pearce3@gmail.com>
2019-07-08 17:51:07 +0100

    Made Image_Seperation Functional

--- Merger_Filter.py
+++ Merger_Filter.py
@@ -13,11 +13,12 @@ ids_to_drop = ex_set1.OBJID_1.append(ex_set2.OBJID_2)
 # drop out matching pairs from the master table
 # ~ returns the compliment of the operation
 new_expert = expert_data[~expert_data.OBJID.isin(ids_to_drop)]
-
+new_merger = merger_data[merger_data.IN_T2_1 == 1]
+new_merger = new_merger[new_merger.IN_T2_2 == 0]
 # check the output has been moded
 print(len(expert_data.index))
 print(len(new_expert.index))
 
 # write out this modded expert list
-
-new_expert.to_csv("../Data/GZ1_Trunc_Expert.csv", header=False)
+new_merger.to_csv("../Data/GZ1_Trunc_Merger.csv", index=False)
+new_expert.to_csv("../Data/GZ1_Trunc_Expert.csv", index=False)
---
+++ Oversampler.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+merger = pd.read_csv("../Data/GZ1_Trunc_Merger.csv")
+
